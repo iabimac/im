04090fc0fdb51fcecb9b4484c0312c795b32df56
iabimac <li.abimael.aceves@gmail.com>
2024-07-02 18:50:54 -0600

    Added a sleep to test creation

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,11 @@
+resource "time_sleep" "wait_4200_seconds" {
+  create_duration = "4200s"
+}
+
 resource "google_compute_disk" "default" {
   project = "reyaceves"
   name  = "test-disk"
   type  = "pd-ssd"
   zone  = "us-central1-a"
+  depends_on = [time_sleep.wait_4200_seconds]
 }
